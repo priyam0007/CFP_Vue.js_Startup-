8a6682dbe991c30a1b5734b1dd0c865dfbdc844d
Your Name <you@example.com>
2022-06-21 12:01:50 +0530

    Added: How to Storing datas in arraylist with using vif Project

---
+++ arraylistvif/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <div id="app">
+    <h3>Do you like tacos?</h3>
+
+    <input v-model="tacos" value="yes" type="radio" id="yes" />
+    <label for="yes"> yes</label><br />
+
+    <input v-model="tacos" value="no" type="radio" id="no" />
+    <label for="no"> no</label><br />
+
+    <p v-if="tacos === 'yes'" class="thumbs">👍</p>
+    <p v-else-if="tacos === 'no'">you're a monster</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+       tacos: ''
+    }
+  }
+}
+</script>
