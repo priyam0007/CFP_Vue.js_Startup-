c1a9bd6f18ef3102b1929a35661b6d32881bd1dc
Your Name <you@example.com>
2022-06-21 12:05:11 +0530

    Added: using vfor in Project

---
+++ vfor/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <div id="app">
+  <ul>
+    <li v-for="num in 5" :key="num">
+      {{ num }}
+    </li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
