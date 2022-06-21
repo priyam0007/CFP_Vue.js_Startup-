fe049183e82ef5a00f37ac06bad67209dc47cdf5
Your Name <you@example.com>
2022-06-21 12:00:31 +0530

    Added: How to Storing datas in arraylist with using vbinds Project

---
+++ arraylistvbinds/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ msg }}</h1>
+ <div id="app">
+  <h3>What is your favorite kind of taco?</h3>
+  <textarea v-model="tacos"></textarea>
+  <br>
+  <button :class="[tacos ? activeClass : '']">Let us know!</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      tacos: '',
+      activeClass: 'active'
+    }
+  }
+ }
+</script>
