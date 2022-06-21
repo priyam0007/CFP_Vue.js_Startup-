fb0c1718114231d4aa5eb369d969d492f468053c
Your Name <you@example.com>
2022-06-21 12:04:28 +0530

    Added: adding filters in Project

---
+++ filter/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+ <div id="app">
+  <h2>Tip Calculator</h2>
+  <p><strong>Total: {{ customer1total }}</strong></p>
+  <p>20%: {{ tip20 }}</p>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+     customer1total: 35.43    
+    }
+  },
+  computed: {
+    tip20() {
+      return (this.customer1total*.2).toFixed(2)
+    },
+  }
+}
+</script>
