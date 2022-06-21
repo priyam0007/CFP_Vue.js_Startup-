09e4534ba8af7d65d7106eb8951ea02d8ffe71ce
Your Name <you@example.com>
2022-06-21 11:57:16 +0530

    Added:  Creating helloworld Project

---
+++ helloworld/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <h2>{{Message}} to Hello World</h2>
+  <p>{{message2}}</p>
+  <div id = "app">{{Message1}} Nice to meet vue</div>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0,
+      Message: 'Welcome',
+      Message1: 'Do your Best',
+      message2: 'Vue.js'
+    }
+  }
+}
+</script>
