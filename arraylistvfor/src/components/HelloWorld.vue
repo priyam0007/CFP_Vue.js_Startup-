ca779290e8a3685ef4a7f92009dceb27b6fd31ae
Your Name <you@example.com>
2022-06-21 12:01:15 +0530

    Added: How to Storing datas in arraylist with using vfor Project

---
+++ arraylistvfor/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{ msg }}</h1>
+ <div id="app">
+  <h3>What is your favorite kind of taco?</h3>
+  <textarea v-model="tacos"></textarea>
+  <br>
+  <button v-show="tacos" type="submit">Let us know!</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      tacos: ''
+    }
+   }
+ }
+</script>
