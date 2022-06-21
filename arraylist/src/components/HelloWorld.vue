79cb28f99eec9cf8479348e0b8b200a23406eac8
Your Name <you@example.com>
2022-06-21 11:59:20 +0530

    Added: How to Storing datas in arraylist Project

---
+++ arraylist/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <div id="app">
+  <input v-model="checkedNames" type="checkbox" id="john" value="John"/>
+  <label for="john"> John</label>
+  
+  <input v-model="checkedNames" type="checkbox" id="paul" value="Paul"/>
+  <label for="paul"> Paul</label>
+  
+  <input v-model="checkedNames" type="checkbox" id="george" value="George"/>
+  <label for="george"> George</label>
+  
+  <input v-model="checkedNames" type="checkbox" id="ringo" value="Ringo"/>
+  <label for="ringo"> Ringo</label>
+  <br>
+  <span>Checked names: {{ checkedNames }}</span>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      checkedNames:[]
+    }
+  }
+}
+</script>
